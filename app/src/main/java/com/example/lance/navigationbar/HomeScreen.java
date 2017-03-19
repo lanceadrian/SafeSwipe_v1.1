3ac55f01ac7cf06e94496777a6395fa2738950a2
Lance Adrian S. Plantilla <lanceadrian.plantilla@gmail.com>
2017-03-19 16:28:54 +0800

    Pushed missing files

---
+++ app/src/main/java/com/example/lance/navigationbar/HomeScreen.java
@@ -0,0 +1,36 @@
+package com.example.lance.navigationbar;
+
+import android.app.Fragment;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+/**
+ * Created by Lance on 3/12/2017.
+ */
+public class HomeScreen extends Fragment {
+
+    View myView;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.home_screen, container, false);
+        return myView;
+    }
+}
